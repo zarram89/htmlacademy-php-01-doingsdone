d00994073d7842d48c70ac596610c90fd394d6dc
Ramil <zarram89@gmail.com>
2026-09-03 12:12:06 +0500

    2.6. Пишем первую функцию

--- index.php
+++ index.php
@@ -48,6 +48,19 @@ $tasks = [
     'completed' => false
   ]
 ];
+
+function countTasksByProject($tasks, $project) {
+  $count = 0;
+
+  foreach ($tasks as $task) {
+    if ($task['project'] === $project) {
+      $count++;
+    }
+  }
+
+  return $count;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -92,7 +105,9 @@ $tasks = [
                         <?php foreach ($projects as $project): ?>
                         <li class="main-navigation__list-item">
                             <a class="main-navigation__list-item-link" href="#"><?= $project; ?></a>
-                            <span class="main-navigation__list-item-count">0</span>
+                            <span class="main-navigation__list-item-count">
+                                <?= countTasksByProject($tasks, $project); ?>
+                            </span>
                         </li>
                         <?php endforeach; ?>
                     </ul>
